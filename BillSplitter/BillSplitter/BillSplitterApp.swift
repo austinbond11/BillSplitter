9410fa5d9a09e31a1de673cecaa17773b3b9ec2b
Austin Bond <63441686+austinbond11@users.noreply.github.com>
2024-06-11 12:04:28 +0300

    Project Files

---
+++ BillSplitter/BillSplitter/BillSplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillSplitterApp.swift
+//  BillSplitter
+//
+//  Created by Austin Bond on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BillSplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
